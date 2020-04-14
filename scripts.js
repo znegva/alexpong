62854b93fb5b841ce7276da01edd5e5b07877f6d
Martin Kausche <mail@martinkausche.de>
2020-04-14 19:04:08 +0200

    Vorbereitung des Spielfeldes mit Hilfe von zuvor definierten Konstanten nur im JavaScript

--- scripts.js
+++ scripts.js
@@ -1,45 +1,65 @@
+const SPIELFELDBREITE = 600;
+const SPIELFELDHOEHE = 400;
+
+const PADDELBREITE = 7;
+const PADDELHOEHE = 40;
+
+const BALLDURCHMESSER = 10;
+
+
 function lassDenBallRollen(){
     var ball = document.getElementById("ball");
         
     setInterval(function(){
-            bewegeNachUnten(ball, 8);
-        }, 2222);
-        setInterval(function(){
-            bewegeNachRechts(ball, 4);
-        }, 800);
-        setInterval(function(){
-            bewegeNachOben(ball, 7);
-        }, 3500);
-        setInterval(function(){
-            bewegeNachLinks(ball, 6);
-        }, 3000);
+        bewegeNachUnten(ball, 8);
+    }, 2222);
+    setInterval(function(){
+        bewegeNachRechts(ball, 4);
+    }, 800);
+    setInterval(function(){
+        bewegeNachOben(ball, 7);
+    }, 3500);
+    setInterval(function(){
+        bewegeNachLinks(ball, 6);
+    }, 3000);
 }
 
 /**
- * eine Funktion die dazu dient das Spielfeld immer korrekt vorzubereiten
- * dazu gehört z.B. den Ball in der Mitte zu positionieren, und die paddel wieder mittig aufzustellen
+ * Eine Funktion die dazu dient das Spielfeld immer korrekt vorzubereiten,
+ * dazu gehört z.B. den Ball in der Mitte zu positionieren, und die paddel wieder mittig aufzustellen.
+ * Wir stellen hier auch die größe des spielfeldes, der Paddel und des Balles ein,
+ * die Werte dazu nehmen wir uns aus vorher definierten Konstanten.
  */
 function spielfeldVorbereiten(){
+    var spielfeld = document.getElementById("spielfeld");
+    spielfeld.style.width = SPIELFELDBREITE;
+    spielfeld.style.height = SPIELFELDHOEHE;
+
     //den ball in die Mitte positionieren
     var ball = document.getElementById("ball");
-    ball.style.top = "195px";
-    ball.style.left = "295px";
+    ball.style.width = BALLDURCHMESSER;
+    ball.style.height = BALLDURCHMESSER;
+    ball.style.top = (SPIELFELDHOEHE - BALLDURCHMESSER) / 2;
+    ball.style.left = (SPIELFELDBREITE - BALLDURCHMESSER) / 2;
 
     //die paddel positionieren
     var paddelLinks = document.getElementById("paddelLinks");
-    paddelLinks.style.left = "3px";
-    paddelLinks.style.top = "185px";
+    paddelLinks.style.width = PADDELBREITE;
+    paddelLinks.style.height = PADDELHOEHE;
+    paddelLinks.style.left = (10 - PADDELBREITE);
+    paddelLinks.style.top = (SPIELFELDHOEHE - PADDELHOEHE) /2;
 
     var paddelRechts = document.getElementById("paddelRechts");
-    paddelRechts.style.right = "3px";
-    paddelRechts.style.top = "185px";
+    paddelRechts.style.width = PADDELBREITE;
+    paddelRechts.style.height = PADDELHOEHE;
+    paddelRechts.style.right = (10 - PADDELBREITE);
+    paddelRechts.style.top = (SPIELFELDHOEHE - PADDELHOEHE) /2;
 }
 
 /*
     ein paar ganz allgemeine Funktionen
 */
 function bewegeNachRechts(element, wert){
-    console.log(element.style.left);
     element.style.left = (parseInt(element.style.left) + wert) + "px";
 }
 function bewegeNachLinks(element, wert){
